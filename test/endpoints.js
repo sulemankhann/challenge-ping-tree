37325f5465e83e4c4546c60de5e054c6ed9fc8b5
Suleman Khan <sulemank97@gmail.com>
2021-06-08 21:20:49 +0500

    added all the required endpoints & add some test case for get method end points

--- test/endpoints.js
+++ test/endpoints.js
@@ -15,3 +15,25 @@ test.serial.cb('healthcheck', function (t) {
     t.end()
   })
 })
+
+test.serial.cb('get all targets', function (t) {
+  var url = '/api/targets'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
+
+test.serial.cb('get single target', function (t) {
+  var url = '/api/targets/1'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
